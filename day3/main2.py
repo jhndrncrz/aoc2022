4f6787037690de300e1b80ae5b79734fc5d7a182
John Adrian Cruz <jhndrncrz@gmail.com>
2022-12-05 21:27:02 +0800

    TODO: Day 4

--- day3/main2.py
+++ day3/main2.py
@@ -1,28 +1,29 @@
+def main():
+    common_items = []
+    group = []
+    with open("input.txt") as file:
+        for line in file:
+            line = line.strip()
+            group.append(line)
+            if len(group) == 3:
+                for item in group[0]:
+                    if item in group[1] and item in group[2]:
+                        common_items.append(item)
+                        break
+                group.clear()
+            
+    common_items = [topriority(i) for i in common_items]
+    print(common_items)
+    print(sum(common_items))         
+
+
 def topriority(item):
     if item.isupper():
         return ord(item) - 38
     if item.islower():
         return ord(item) - 96
 
-def main():
-    common_items = []
-    with open("input.txt") as file:
-        for line in file:
-            line = line.strip()
-            items_per_comp = len(line)//2
-            first_comp = line[:items_per_comp]
-            second_comp = line[items_per_comp:]
-            for i in first_comp:
-                if i in second_comp:
-                    common_items.append(i)
-                    break
-    common_items = [topriority(i) for i in common_items]
-    print(sum(common_items))
-    ## TODO: Day 3, Challenge 2
-
-            
-
-            
 
 if __name__ == "__main__":
     main()
+
