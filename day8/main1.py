690ab997e5c34a27317e2a242e5f2b684498434e
John Adrian Cruz <jhndrncrz@gmail.com>
2022-12-11 14:57:16 +0800

    Day 8: part 1 done, to do part 2

--- day8/main1.py
+++ day8/main1.py
@@ -5,38 +5,49 @@ def is_visible(grid: list[list[int]], r:int, c:int) -> bool:
     """
     Checks if tree is visible by comparing it to the edges and the height of the trees with the same column and row
     """
+    #print(f"{grid[r][c]} ({r},{c})", end = " ")
     # Checks if it is an edge tree
     if r == 0 or r == len(grid) - 1 or c == 0 or c == len(grid[0]) - 1:
+        #print(f"is an edge tree")
         return True
     
     visibility = [True] * 4
 
-    # Checks for visibility from top
-    for i in range(len(grid[0:r])):
-        if grid[r][c] < grid[i][c]:
+    ## Check visibility from top, iterate on rows, fix column
+    for i in range(0, r):
+        if grid[r][c] <= grid[i][c]:
+            #print(f"not visible from top since {grid[r][c]} <= {grid[i][c]}")
             visibility[0] = False
     
-    # Checks for visibility from bottom
-    for i in range(len(grid[r:])):
-        if grid[r][c] < grid[i][c]:
+    ## Check visibility from bottom, iterate on rows, fix column
+    for i in range(r+1, len(grid)):
+        if grid[r][c] <= grid[i][c]:
+            #print(f"not visible from bottom since {grid[r][c]} <= {grid[i][c]}")
             visibility[1] = False
 
-    # Checks for visibility from left
-    for i in range(len(grid[r][:c])):
-        if grid[r][c] < grid[r][i]:
+    ## Check visibility from left, iterate on columns, fix row
+    for i in range(0, c):
+        if grid[r][c] <= grid[r][i]:
+            #print(f"not visible from left since {grid[r][c]} <= {grid[r][i]}")
             visibility[2] = False
     
-    # Checks for visibility from right
-    for i in range(len(grid[r][c:])):
-        if grid[r][c] < grid[r][i]:
+    ## Check visibility from right, iterate on columns, fix row
+    for i in range(c+1, len(grid[r])):
+        if grid[r][c] <= grid[r][i]:
+            #print(f"not visible from right since {grid[r][c]} <= {grid[r][i]}")
             visibility[3] = False
-
-    #for tree in grid[x]:
-    #    if grid[x][y] < tree:
-    #        return False
-    #for row in grid:
-    #    if grid[x][y] < row[y]:
-    #        return False
+    
+    if any(visibility):
+        visiblefrom = ""
+        if visibility[0]:
+            visiblefrom += "top "
+        if visibility[1]:
+            visiblefrom += "bottom "
+        if visibility[2]:
+            visiblefrom += "left "
+        if visibility[3]:
+            visiblefrom += "right"
+        #print(f"is visible from {visiblefrom}")
     return any(visibility)
 
 
@@ -52,8 +63,6 @@ def main():
         for j in range(len(grid[i])):
             if is_visible(grid, i, j):
                 count += 1
-                print(grid[i][j], is_visible(grid, i, j))
-        print()
     print(count)
 
 if __name__ == "__main__":
