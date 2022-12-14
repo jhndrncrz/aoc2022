ce6a93d3aa1520eefead1c4f9be9da0f1207d0c4
John Adrian Cruz <jhndrncrz@gmail.com>
2022-12-14 13:55:51 +0800

    Day 10: formatted with black

--- day10/main1.py
+++ day10/main1.py
@@ -1,14 +1,16 @@
 from typing import Any
 
-def parse_line(line:str) -> tuple[str, Any]:
+
+def parse_line(line: str) -> tuple[str, Any]:
     command: list[Any] = line.strip().split()
     if len(command) == 2:
         command[1] = int(command[1])
-    else: 
+    else:
         command.append(None)
     return tuple(command)
 
-def run_cycle(cycle:int, x:int, line:str, milestones:list[int]) -> tuple[Any, ...]:
+
+def run_cycle(cycle: int, x: int, line: str, milestones: list[int]) -> tuple[Any, ...]:
     command = parse_line(line)
     if command[0] == "noop":
         cycle += 1
@@ -27,16 +29,19 @@ def run_cycle(cycle:int, x:int, line:str, milestones:list[int]) -> tuple[Any, ..
             milestones.append(signal_strength(x, cycle))
     return cycle, x, milestones
 
-def check_cycle(cycle:int) -> bool:
+
+def check_cycle(cycle: int) -> bool:
     return cycle % 40 == 20
 
-def signal_strength(x:int, cycle:int) -> int:
-    return x*cycle
+
+def signal_strength(x: int, cycle: int) -> int:
+    return x * cycle
+
 
 def main():
-    cycle:int = 1
-    x:int = 1
-    milestones:list[int] = []
+    cycle: int = 1
+    x: int = 1
+    milestones: list[int] = []
     with open("input.txt") as file:
         while cycle <= 220:
             line = file.readline()
--- day10/main2.py
+++ day10/main2.py
@@ -1,46 +1,47 @@
 from typing import Any
 
-def parse_line(line:str) -> tuple[str, Any]:
+
+def parse_line(line: str) -> tuple[str, Any]:
     command: list[Any] = line.strip().split()
     if len(command) == 2:
         command[1] = int(command[1])
-    else: 
+    else:
         command.append(None)
     return tuple(command)
 
 
-def run_cycle(cycle:int, x:int, line:str, screen:list[int]) -> tuple[Any, ...]:
+def run_cycle(cycle: int, x: int, line: str, screen: list[int]) -> tuple[Any, ...]:
     command = parse_line(line)
     print(f"Cycle {cycle}: x = {x}")
     cycle += 1
-    if (cycle - 1) % 40 in range(x-1, x+2):
+    if (cycle - 1) % 40 in range(x - 1, x + 2):
         screen[(cycle - 1)] += 1
-    
+
     if command[0] == "noop":
         pass
-        
+
     elif command[0] == "addx":
         x += command[1]
         print(f"Cycle {cycle}: x = {x}")
         cycle += 1
-        if (cycle - 1) % 40 in range(x-1, x+2):
+        if (cycle - 1) % 40 in range(x - 1, x + 2):
             screen[(cycle - 1)] += 1
-        
+
     return cycle, x, screen
 
 
 def main():
-    cycle:int = 1
-    x:int = 1
-    screen: list[int] = [0]*240
+    cycle: int = 1
+    x: int = 1
+    screen: list[int] = [0] * 240
     with open("input.txt") as file:
         while cycle <= 240:
             line = file.readline()
             if not line:
                 break
             cycle, x, screen = run_cycle(cycle, x, line, screen)
-    
-    screen_new = ''.join([str(i) for i in screen])
+
+    screen_new = "".join([str(i) for i in screen])
 
     screen_new = screen_new.replace("0", ".").replace("1", "#")
 
@@ -50,6 +51,7 @@ def main():
     print(screen_new[120:160])
     print(screen_new[160:200])
     print(screen_new[200:240])
+
 
 if __name__ == "__main__":
     main()
